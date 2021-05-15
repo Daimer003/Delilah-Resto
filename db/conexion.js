178df750adf5d92d2acaf4e8d92de20616f3976e
Daimer003 <daimer003@gmail.com>
2021-05-15 15:53:15 -0500

    Conexion con la base de datos ok

--- db/conexion.js
+++ db/conexion.js
@@ -1,10 +1,22 @@
 const Sequelize = require('sequelize');
-const path = 'mysql://root:root@localhost:3306/Delilah-Resto';
+const NODE_ENV = process.env.NODE_ENV || 'developer';
+require('dotenv').config({
+   path: `./.env.${NODE_ENV}`
+});
 
+const conexion = new Sequelize(
+   'Delilah-Resto',
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+       host: process.env.DB_HOST,
+       dialect: process.env.DB_DIALECT
+    }
 
-const conexion = new Sequelize(path);
+);
 
-conexion.authenticate()
+conexion
+        .authenticate()
 .then(()=> {
    console.log('Conexion exitosa');
 })
