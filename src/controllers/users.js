c656b1d1382ea77440dd2691ee8a8bbd36e5241f
daimer003 <daimer003@gmail.com>
2021-06-01 07:36:25 -0500

    optimizando endpoints de usuarios

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,59 +1,61 @@
-const usersModel = require('../models/users');
+const usersModel = require("../models/users");
 
 class User {
-    static async create(req, res, next) {
-        // 1 Optener los datos
-       const {body} = req;
-       
-       try {
-       //2 Crear usuario con los datos recibidos 
-        const userCreated = await usersModel.create({
-            name: body.name,
-            password: body.password,
-            roles_id: body.roles_id
-        });
-        //3 Enviar menjade de usuario creado
-        return res.status(201).json({
-            message: `user ${userCreated.name} created`
-        })
-        //4 Si no enviar error    
-       } catch (error) {
-           return res.status(500).json({message: 'Error of user'});
-       }
-     
+  //2 Crear usuario con los datos recibidos
+  static async create(req, res, next) {
+    const { body } = req;
+    try {
+      const userCreated = await usersModel.create({
+        name: body.name,
+        password: body.password,
+        roles_id: body.roles_id,
+      });
+      return res.status(201).json({
+        message: `user ${userCreated.name} created`,
+      });
+    } catch (error) {
+      return res.status(500).json({ message: "Error of user" });
     }
+  }
 
-    //Recibir todos los usuarios 
-    static async getAll(req, res) {
-        // 1 Optener los datos
-    
-       const usuarios = await usersModel.findAll()
-       res.status(200).json(usuarios);
+  //Recibir todos los usuarios
+  static async getAll(req, res) {
+    try {
+      const usuarios = await usersModel.findAll();
+      res.status(200).json(usuarios);
+    } catch {
+      return res.status(500).json({ message: "users query error" });
     }
+  }
 
-    //Actualizar usuario
-    static async updateUser(req, res, next) {
-        // 1 Optener id del usuario
-        const {body} = req;
-        const userId = await req.params.id
-        const updateUser = await usersModel.update({ name: body.name, password: body.password }, {where: {id: userId}})
-         res.status(200).json({updateUser});
-       
-
+  //Actualizar usuario
+  static async updateUser(req, res, next) {
+    try {
+      const { body } = req;
+      const userId = await req.params.id;
+      const updateUser = await usersModel.update(
+        { name: body.name, password: body.password },
+        { where: { id: userId } }
+      );
+      res.status(200).json({ updateUser });
+    } catch {
+      return res.status(500).json({ message: "update error" });
     }
-
-    static async deleteUser(req, res, next) {
-        // 1 Optener id del usuario
-        const userId = await req.params.id
-        await usersModel.destroy({
-            where: {
-              id: userId,
-            }
-          });
-
+  }
+  //  Eliminar usuario
+  static async deleteUser(req, res, next) {
+    try {
+      const userId = await req.params.id;
+      await usersModel.destroy({
+        where: {
+          id: userId,
+        },
+      });
+      return res.status(200).json({ message: "Delete user" });
+    } catch {
+      return res.status(500).json({ message: "Delete error" });
     }
-   
+  }
 }
-
 
 module.exports = User;
